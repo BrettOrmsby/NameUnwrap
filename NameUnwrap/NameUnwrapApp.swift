b73d98d437e3cca24df7b58e6187e0ebd9528296
BrettOrmsby <brettwormsby@outlook.com>
2023-07-03 17:28:42 -0400

    Initial Commit

---
+++ NameUnwrap/NameUnwrapApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameUnwrapApp.swift
+//  NameUnwrap
+//
+//  Created by Brett Ormsby on 2023-07-03.
+//
+
+import SwiftUI
+
+@main
+struct NameUnwrapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
